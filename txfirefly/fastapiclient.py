8118af3faa84e077f6d83437258479bc1e6b0fba
Joe <jackwmj12@163.com>
2021-03-09 12:33:52 +0800

    v1.0.12 .临时提交

---
+++ txfirefly/fastapiclient.py
@@ -0,0 +1,33 @@
+# !/usr/bin/env Python3
+# -*- coding: utf-8 -*-
+# @Author   : joe lin
+# @FILE     : fastapiclient.py
+# @Time     : 2021-03-08 23:36
+# @Software : tiseal_app
+# @Email    : jackwmj12@163.com
+# @Github   : 
+# @Desc     : 
+#            ┏┓      ┏┓
+#          ┏┛┻━━━┛┻┓
+#          ┃      ━      ┃
+#          ┃  ┳┛  ┗┳  ┃
+#          ┃              ┃
+#          ┃      ┻      ┃
+#          ┗━┓      ┏━┛
+#              ┃      ┃
+#              ┃      ┗━━━┓
+#              ┃              ┣┓
+#              ┃              ┏┛
+#              ┗┓┓┏━┳┓┏┛
+#                ┃┫┫  ┃┫┫
+#                ┗┻┛  ┗┻┛
+#                 神兽保佑，代码无BUG!
+#
+#
+#
+from txfirefly.client import ClientNode
+
+class FastApiRpcClient(ClientNode):
+	
+	def run(self):
+		self.connectMaster()
